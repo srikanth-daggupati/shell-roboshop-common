2b61706f914a3de90ef6a89b012c483955139bfa
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-07 17:13:22 -0600

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
- 
- source ./common.sh
+
+source ./common.sh
 app_name=catalogue
 
- check_root
- app_setup
- nodejs_setup
- systemd_setup
+check_root
+app_setup
+nodejs_setup
+systemd_setup
 
 # Loading data into MongoDB
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
@@ -15,11 +15,8 @@ dnf install mongodb-mongosh -y &>>$LOGS_FILE
 INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOGS_FILE
     VALIDATE $? "Loading products"
 else
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | Products already loaded ... $Y SKIPPING $N"
 fi
-
-systemctl restart $app_name
-VALIDATE $? "Restarting $app_name"
